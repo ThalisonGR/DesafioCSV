8d08bf8eebf7636dd2828857e8b25856d981673d
Thalison <Tec.thalison@gmail.com>
2024-10-06 10:05:43 -0300

    init: Inicializando projeto

---
+++ src/main/java/com/ifrs/DesafioCSV/DesafioCsvApplication.java
@@ -0,0 +1,13 @@
+package com.ifrs.DesafioCSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifrs/DesafioCSV/DesafioCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrs.DesafioCSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
